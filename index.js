0f9c784dc91d2295641299e5532306960459261e
Kishan Gopal Modi <kishangopalmodi@gmail.com>
2023-01-05 11:25:19 +0530

    Updated the script for the utterence

--- index.js
+++ index.js
@@ -1,64 +1,152 @@
-// function speak() {
-//   const text = document.querySelector(".text").innerText;
-//   console.log(text);
-//   let utterance = new SpeechSynthesisUtterance(text);
-//   speechSynthesis.speak(utterance);
-// }
-// window.onload = () => {
-//   // (A) TTS SUPPORTED
-//   if ("speechSynthesis" in window) {
-//     // (B) GET HTML ELEMENTS
-//     // (C) POPULATE AVAILABLE VOICES
-//     // (D) SPEAK
-//     // (X) TTS NOT SUPPORTED
-//   } else {
-//     alert("Text-to-speech is not supported on your browser!");
-//   }
-// };
+// (A) TTS SUPPORTED
+if ("speechSynthesis" in window) {
+  let synth = window.speechSynthesis;
+  synth.cancel();
 
-$(document).ready(function () {
-  // (A) TTS SUPPORTED
-  if ("speechSynthesis" in window) {
-    speechSynthesis.cancel();
+  var speechUtteranceChunker = function (utt, settings, callback) {
+    settings = settings || {};
+    var newUtt;
+    var txt =
+      settings && settings.offset !== undefined
+        ? utt.text.substring(settings.offset)
+        : utt.text;
+    if (utt.voice && utt.voice.voiceURI === "native") {
+      // Not part of the spec
+      newUtt = utt;
+      newUtt.text = txt;
+      newUtt.addEventListener("end", function () {
+        if (speechUtteranceChunker.cancel) {
+          speechUtteranceChunker.cancel = false;
+        }
+        if (callback !== undefined) {
+          callback();
+        }
+      });
+    } else {
+      var chunkLength = (settings && settings.chunkLength) || 160;
+      var pattRegex = new RegExp(
+        "^[\\s\\S]{" +
+          Math.floor(chunkLength / 2) +
+          "," +
+          chunkLength +
+          "}[.!?,]{1}|^[\\s\\S]{1," +
+          chunkLength +
+          "}$|^[\\s\\S]{1," +
+          chunkLength +
+          "} "
+      );
+      var chunkArr = txt.match(pattRegex);
 
-    let vmsg = document.getElementById("read-from-here");
+      if (
+        chunkArr == null ||
+        chunkArr[0] === undefined ||
+        chunkArr[0].length <= 2
+      ) {
+        //call once all text has been spoken...
+        if (callback !== undefined) {
+          callback();
+        }
+        return;
+      }
+      var chunk = chunkArr[0];
+      newUtt = new SpeechSynthesisUtterance(chunk);
+      var x;
+      for (x in utt) {
+        if (utt.hasOwnProperty(x) && x !== "text") {
+          newUtt[x] = utt[x];
+        }
+      }
+      newUtt.addEventListener("end", function () {
+        if (speechUtteranceChunker.cancel) {
+          speechUtteranceChunker.cancel = false;
+          return;
+        }
+        settings.offset = settings.offset || 0;
+        settings.offset += chunk.length;
+        speechUtteranceChunker(utt, settings, callback);
+      });
+    }
 
-    let msg = new window.SpeechSynthesisUtterance(vmsg.innerText);
+    if (settings.modifier) {
+      settings.modifier(newUtt);
+    }
+    console.log(newUtt); //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
+    //placing the speak invocation inside a callback fixes ordering and onend issues.
+    setTimeout(function () {
+      speechSynthesis.speak(newUtt);
+    }, 0);
+  };
 
-    var speak = () => {
-      speechSynthesis.speak(msg);
+  // Changes done by Kishan - 19-12-2022
+
+  window.onload = function () {
+    var utterance = new SpeechSynthesisUtterance();
+
+    document.querySelector("#play").addEventListener("click", function () {
+      document.querySelector(".audioControls").classList.add("played");
+      const containerToRead = document.querySelector(".the_blog_to_read");
+      const clonedText = containerToRead.cloneNode(true);
+
+      // remove unwantedDivs before converting it to text
+      if (clonedText.querySelector(".addtoany_content_top")) {
+        clonedText.removeChild(
+          clonedText.querySelector(".addtoany_content_top")
+        );
+      }
+      if (clonedText.querySelector(".addtoany_content_bottom")) {
+        clonedText.removeChild(
+          clonedText.querySelector(".addtoany_content_bottom")
+        );
+      }
+
+      var myLongText = clonedText.innerText;
+
+      utterance.text = myLongText;
+      var voiceArr = speechSynthesis.getVoices();
+
+      utterance.voice = voiceArr[0];
+      speechUtteranceChunker(
+        utterance,
+        {
+          chunkLength: 150,
+        },
+        function () {
+          //some code to execute when done
+          console.log("done");
+        }
+      );
+    });
+
+    utterance.onerror = (event) => {
+      console.error(
+        `An error has occurred with the speech synthesis: ${event.error}`
+      );
     };
 
-    document.querySelector(".play-button").addEventListener("click", speak);
+    document.querySelector("#pause").addEventListener("click", function () {
+      document.querySelector(".audioControls").classList.remove("resumed");
+      document.querySelector(".audioControls").classList.add("paused");
+      synth.pause();
+    });
 
-    document
-      .querySelector(".pause-button")
-      .addEventListener("click", function () {
-        speechSynthesis.pause();
-      });
+    document.querySelector("#resume").addEventListener("click", function () {
+      document.querySelector(".audioControls").classList.remove("paused");
+      document.querySelector(".audioControls").classList.add("resumed");
 
-    document
-      .querySelector(".resume-button")
-      .addEventListener("click", function () {
-        window.speechSynthesis.resume();
-      });
+      synth.resume();
+    });
 
-    document
-      .querySelector(".rewind-button")
-      .addEventListener("click", function () {
-        console.log(window.speechSynthesis, msg);
-        // msg.currentTime -= 10;
-      });
+    document.querySelector("#stop").addEventListener("click", function () {
+      document.querySelector(".audioControls").classList.remove("played");
+      document.querySelector(".audioControls").classList.remove("paused");
 
-    document
-      .querySelector(".forward-button")
-      .addEventListener("click", function () {
-        // msg.currentTime += 10;
-      });
-  }
+      speechUtteranceChunker.cancel = true;
+      synth.cancel();
+    });
+  };
+}
 
-  // (X) TTS NOT SUPPORTED
-  else {
-    alert("Text-to-speech is not supported on your browser!");
-  }
-});
+// (X) TTS NOT SUPPORTED
+else {
+  alert("Text-to-speech is not supported on your browser!");
+}
